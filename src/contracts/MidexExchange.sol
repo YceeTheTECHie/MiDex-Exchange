f2d2f857b209363f38c7c879e605194152c9b546
Boluwatife Ayodele <waplurd1@gmail.com>
2021-09-28 00:36:41 +0100

    wrote buy token functionality

--- src/contracts/MidexExchange.sol
+++ src/contracts/MidexExchange.sol
@@ -1,6 +1,16 @@
 pragma solidity ^0.5.0;
 
 contract MidexExchange {
-    string public name = "Midex Exchange"
-   
+    string public name = "Midex Exchange";
+    Token public token;
+    uint256 public rate = 100;
+
+    constructor(Token _token) public {
+        token = _token;
+    }
+
+    function buyCoins() public payable {
+        uint amountToken = msg.value * 100;
+        token.transfer(msg.sender,totalAmount);
+    } 
 }
